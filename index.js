0dfeb355c6dae7bba98ffc48aaf6835072af82d9
isaac <isaac.ingles@gmail.com>
2020-01-06 15:43:00 -0700

    simple server running

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.use('/', (req, res, next)=>{
+    console.log(`New request was recieved at ${new Date().toLocaleString()}`)
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
